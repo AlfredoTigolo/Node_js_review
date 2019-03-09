ea9a7f69ff17e2bc1421175e2867f07969bef1ce
AlfredoTigolo <alfredo.tigolo@yahoo.com>
2019-03-09 06:41:15 -0800

    changed get commands
    refractor code and added new get url

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/ISCC', (req, res) => res.send('Hello World from Southside!'))
+app.get('/guest', (req, res) => res.send('Hello World from Southside!'))
+app.get('/user/:dogs', function(req, res){
+    const dogs = req.params.dogs;
+
+    //res.send('dogs ' + req.params.dogs);
+    res.send('dogs ' + dogs);
+  });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
